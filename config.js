3a4ed2014ab0fc97db23f9c3320ef6b177faa694
sooraj-4 <111184434+sooraj-4@users.noreply.github.com>
2022-08-21 17:17:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,19 +18,19 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '917034685001' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
+global.owner = ['917034685001',''] //මෙතන number එක.
+global.premium = ['917034685001','917034685001'] //මෙතන number එක.
 global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
+global.ownername = 'ꪶᴄ͢ʜʀᷧɪᷟsᴛͥʏ ͢ꫂsᴇʀ⋆🜲' //මෙතන නම.
+global.ownername = 'ꪶᴄ͢ʜʀᷧɪᷟsᴛͥʏ ͢ꫂsᴇʀ⋆🜲' //මෙතන නම.
 global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
 global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://i.ibb.co/BBB6Rxn/Vihanga-MD.png' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.packname = 'ꪶᴄ͢ʜʀᷧɪᷟsᴛͥʏ ͢ꫂsᴇʀ⋆🜲' //මෙතන නම.
+global.cap = '*By ꪶᴄ͢ʜʀᷧɪᷟsᴛͥʏ ͢ꫂsᴇʀ⋆🜲*' //මෙතන නම.
+global.img = 'https://i.imgur.com/JHFaMT5.jpeg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
 global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
@@ -52,7 +52,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = 'ꪶᴄ͢ʜʀᷧɪᷟsᴛͥʏ ͢ꫂsᴇʀ⋆🜲' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
